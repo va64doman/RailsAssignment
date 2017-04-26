a5e462b402144c0cc592710e6a625c583415119c
va64doman <va64doman@hotmail.com>
2017-04-26 16:57:53 +0800

    Final Change

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -1,5 +1,5 @@
 module CourseHelper
 	def option_for_study
-		options_for_select([["Full Time", "Full Time"], ["Part Time", "Part Time"], ["Online Study", "Online Study"]])
+		options_for_select([["Full Time", "Full Time"], ["Part Time", "Part Time"], ["Online Study", "Online Study"]], @course.study)
 	end
 end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,5 @@
 module UserHelper
 	def option_for_admin
-		options_for_select([["Student", 1], ["Lecturer", 2], ["Admin", 3]])
+		options_for_select([["Student", 1], ["Lecturer", 2], ["Admin", 3]], @user.admin)
 	end
 end
