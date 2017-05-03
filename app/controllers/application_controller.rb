adaded34275a8d1e867111301ddbc354ddcf6164
va64doman <va64doman@hotmail.com>
2017-05-03 10:49:07 +0800

    Update security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,16 @@ class ApplicationController < ActionController::Base
 
   #To avoid user seeing cache page, it responses that there will not be any cache and should not be stored.
   before_action :set_as_private
+  before_action :check_page
 
   def set_as_private
   	response.headers['Cache-Control'] = 'no-cache, no-store'
+  end
+
+  #Check if the user has already login and try to go to login, then redirect to course index
+  def check_page
+  	if request.path == "/login" && logged_in?
+  		redirect_to courses_index_url
+  	end
   end
 end
